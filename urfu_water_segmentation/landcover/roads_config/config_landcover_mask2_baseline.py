94fc9fe7d6f83d55490337982dc0649b9698c4be
студент0038 <s0038@umtnew.imm.uran.ru>
2025-09-26 12:47:15 +0500

    fix baseline

--- urfu_water_segmentation/landcover/roads_config/config_landcover_mask2_baseline.py
+++ urfu_water_segmentation/landcover/roads_config/config_landcover_mask2_baseline.py
@@ -1,7 +1,7 @@
 # для выбора модели, расписания необходимо наследовать один из файлов из репозитория mmsegmentation
 # базовые файлы для наследования можно посмотреть по пути mmsegmentation/configs/_base_/
 _base_ = [
-    '../../configs/mask2former/mask2former_swin-t_8xb2-160k_ade20k-512x512.py',
+    '../../../configs/mask2former/mask2former_swin-t_8xb2-160k_ade20k-512x512.py',
 ]
 
 # ----------------------------------------------------------------
@@ -17,10 +17,10 @@ data_root = '/misc/home6/m_imm_freedata/Segmentation/landcover.ai_512'
 num_classes = 2
 # Размер изображения, который принимает на вход сеть
 crop_size = (512, 512)
-# Количичество эпох для обучения
+# Количество эпох для обучения
 max_epochs = 100
-# Функция потерь
-loss = dict(type='FocalLoss', class_weight=[0.9, 1.1]) # [фон, таргет] [1.0, 3.0], [0.5, 2.0], [1.0, 5.0]
+# Функция потерь (переменная для удобства, но важно переопределить в decode_head)
+loss = dict(type='FocalLoss', class_weight=[0.9, 1.1]) # [фон, таргет]
 # Размер батча
 batch_size = 16
 gradient_accumulation_steps = 8
@@ -28,9 +28,7 @@ actual_batch_size = batch_size * gradient_accumulation_steps
 # num_workers
 num_workers = 8
 
-# Оптимизатор
-# optimizer = dict(type='SGD', lr=1e-3, momentum=0.9, weight_decay=0.0005)
-# optimizer = dict(type='AdamW', lr=3e-4, weight_decay=0.001)
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
 
 # Параметры логирования 
 experiment_name = f'Mask2_{dataset_type}_{crop_size[0]}_{loss["type"]}_bsize_{actual_batch_size}'
@@ -45,14 +43,30 @@ splits = 'splits'
 pretrained = 'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/swin/swin_small_patch4_window7_224_20220317-7ba6d6dd.pth'  # noqa
 
 depths = [2, 2, 18, 2]
+# сохраняем backbone как есть, но добавляем переопределение head ниже
 model = dict(
     backbone=dict(
         depths=depths, init_cfg=dict(type='Pretrained',
-                                     checkpoint=pretrained)))
+                                     checkpoint=pretrained))
+)
+
+model.update(
+    dict(
+        decode_head=dict(
+            num_classes=num_classes,
+            loss_cls=dict(
+                type='mmdet.CrossEntropyLoss',
+                use_sigmoid=False,
+                loss_weight=2.0,
+                class_weight=[1.0, 3.0]  # фон=1.0, дорога=3.0 — подбирается эмпирически
+            ),
+            loss_mask=dict(type='mmdet.CrossEntropyLoss', use_sigmoid=True, loss_weight=5.0),
+            loss_dice=dict(type='mmdet.DiceLoss', use_sigmoid=True, activate=True, eps=1.0, loss_weight=5.0),
+        )
+    )
+)
 
 # set all layers in backbone to lr_mult=0.1
-# set all norm layers, position_embeding,
-# query_embeding, level_embeding to decay_multi=0.0
 backbone_norm_multi = dict(lr_mult=0.1, decay_mult=0.0)
 backbone_embed_multi = dict(lr_mult=0.1, decay_mult=0.0)
 embed_multi = dict(lr_mult=1.0, decay_mult=0.0)
@@ -75,10 +89,35 @@ custom_keys.update({
     f'backbone.stages.{stage_id}.downsample.norm': backbone_norm_multi
     for stage_id in range(len(depths) - 1)
 })
-# optimizer
-optim_wrapper = dict(
-    paramwise_cfg=dict(custom_keys=custom_keys, norm_decay_mult=0.0))
+# optimizer wrapper (включаем custom_keys)
+optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer, paramwise_cfg=dict(custom_keys=custom_keys, norm_decay_mult=0.0), clip_grad=None)
 
+# ------------------ runtime / logging / scheduler ------------------
+# Логгер и чекпойнты в стиле примера
+log_processor = dict(by_epoch=True)
+
+# простой полиномиальный шедулер (по эпохам) — можно сменить на любой другой
+param_scheduler = [
+    dict(
+        type='PolyLR',
+        eta_min=1e-4,
+        power=0.9,
+        begin=0,
+        end=max_epochs,
+        by_epoch=True)
+]
+
+# train loop — переопределяем, чтобы явно указать макс эпохи и валидацию каждую эпоху
+train_cfg = dict(_delete_=True, max_epochs=max_epochs, type='EpochBasedTrainLoop', val_interval=1)
+
+# default_hooks: логирование, чекпоинты, визуализация (draw=False чтобы не сохранять много картинок автоматически)
+default_hooks = dict(
+    logger=dict(type='LoggerHook', log_metric_by_epoch=True, interval=log_interval),
+    checkpoint=dict(type='CheckpointHook', by_epoch=True, interval=1, max_keep_ckpts=3),
+    visualization=dict(type='SegVisualizationHook', draw=False, interval=500)
+)
+
+# ------------------ даталоадеры и пайплайны ------------------
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', reduce_zero_label=False),
@@ -109,8 +148,8 @@ train_dataloader = dict(
             seg_map_path='train/gt'),
         pipeline=train_pipeline,
         # ann_file=f'{splits}/train.txt'
-        )
     )
+)
 
 val_dataloader = dict(
     batch_size=batch_size,
@@ -125,13 +164,14 @@ val_dataloader = dict(
             seg_map_path='val/gt'),
         pipeline=test_pipeline,
         # ann_file=f'{splits}/val.txt'
-        )
     )
+)
 
 test_dataloader = val_dataloader
 val_evaluator = dict(type='IoUMetric', iou_metrics=['mIoU'])
 test_evaluator = val_evaluator
 
+# ------------------ визуализация ------------------
 vis_backends = [dict(type='LocalVisBackend', scalar_save_file='../../scalars.json', save_dir=work_dir),
                 dict(type='TensorboardVisBackend', save_dir=work_dir)]
 visualizer = dict(type='SegLocalVisualizer', vis_backends=vis_backends, name='visualizer')
