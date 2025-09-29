5d275142793c3df27ef1eed905623c87d1b056b3
Владислав Савенков <vladislavsavenkov@Vladislavs-MacBook-Pro.local>
2025-09-29 20:40:47 +0500

    added script for multi gpu run

--- urfu_water_segmentation/landcover/dist_train.sh
+++ urfu_water_segmentation/landcover/dist_train.sh
@@ -2,14 +2,12 @@
 
 source ../.venv/bin/activate
 
-GPUS=2
-NODE_PARAMS="-p hiperf --gres=gpu:a100:$GPUS --nodelist=tesla-a101 -t 00:15:00"
+GPU_ID="v100" # a100, a101, v100
+GPU_COUNT=8
 
-sbatch -n1 \
-    --cpus-per-task=8 \
-    --mem=45000 \
-    $NODE_PARAMS \
-    --job-name=mmsegm-water \
-    --ntasks=${GPUS} \
-    --ntasks-per-node=${GPUS} \
-    --wrap="python ./train.py ./config_debug.py --launcher slurm"
+sbatch \
+  -p hiperf --nodelist=tesla-${GPU_ID} \
+  --nodes=1 --gres=gpu:${GPU_ID}:${GPU_COUNT} \
+  --cpus-per-task=${GPU_COUNT} --mem=45G -t 9:59:59 -J mmsegm-tree \
+  --wrap="srun torchrun --nproc_per_node=${GPU_COUNT} --master_port=23456 \
+          ./train.py ./config_debug.py --launcher pytorch"
