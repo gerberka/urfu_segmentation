c5e6171b925a48a15867a6811b3d47ba57b2c936
Владислав Савенков <vladislavsavenkov@Vladislavs-MacBook-Pro.local>
2025-09-29 18:32:24 +0500

    test run with existing config and dataset

--- urfu_water_segmentation/landcover/my_train_multi_gpu.sh
+++ urfu_water_segmentation/landcover/my_train_multi_gpu.sh
@@ -1,30 +1,8 @@
-#!/bin/bash
-#SBATCH -p hiperf
-#SBATCH --nodelist=tesla-a100
-#SBATCH --gres=gpu:a100:2
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=45G
-#SBATCH -t 02:00:00
-#SBATCH -J mmsegm-tree
+#!/bin/sh
 
-set -euo pipefail
-export PYTHONUNBUFFERED=1
-export OMP_NUM_THREADS=1
-export MKL_NUM_THREADS=1
-# Диагностика DDP/NCCL (можно выключить позже)
-export TORCH_DISTRIBUTED_DEBUG=DETAIL
-export NCCL_DEBUG=INFO
-export NCCL_IB_DISABLE=1           # если нет Infiniband
-# export NCCL_SOCKET_IFNAME=lo     # single-node иногда помогает
-
-source ../.venv/bin/activate
-cd landcover
-
-GPUS=2
-PORT=$((12000 + RANDOM % 20000))
-
-# важное: именно torchrun и launcher=pytorch
-srun --gpu-bind=closest \
-  torchrun --nproc_per_node=${GPUS} --master_port=${PORT} \
-  ./train.py ./configs_2025_05/config_trees_mask2former.py \
-  --launcher pytorch
+sbatch \
+  -p hiperf --nodelist=tesla-v100 \
+  --nodes=1 --gres=gpu:v100:2 \
+  --cpus-per-task=8 --mem=45G -t 02:00:00 -J mmsegm-tree \
+  --wrap="srun torchrun --nproc_per_node=2 --master_port=23456 \
+          ./train.py ./configs_2025_05/config_trees_mask2former.py --launcher pytorch"
