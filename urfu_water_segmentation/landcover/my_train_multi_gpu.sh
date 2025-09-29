419d8ac12464288a918baac8e5c829bb6e532553
Владислав Савенков <vladislavsavenkov@Vladislavs-MacBook-Pro.local>
2025-09-29 18:52:17 +0500

    test run with existing config and dataset

--- urfu_water_segmentation/landcover/my_train_multi_gpu.sh
+++ urfu_water_segmentation/landcover/my_train_multi_gpu.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 sbatch \
-  -p hiperf --nodelist=tesla-v100 \
-  --nodes=1 --gres=gpu:v100:8 \
+  -p hiperf --nodelist=tesla-a100 \
+  --nodes=1 --gres=gpu:a100:8 \
   --cpus-per-task=8 --mem=45G -t 02:00:00 -J mmsegm-tree \
   --wrap="srun torchrun --nproc_per_node=8 --master_port=23456 \
           ./train.py ./configs_2025_05/config_trees_mask2former.py --launcher pytorch"
