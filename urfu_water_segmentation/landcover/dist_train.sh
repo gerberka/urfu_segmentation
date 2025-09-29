9d7b75e2e5766f1751cf64165b3a2982430825ad
студент0039 <s0039@umtnew.imm.uran.ru>
2025-09-29 12:24:51 +0500

    test run with existing config and dataset

--- urfu_water_segmentation/landcover/dist_train.sh
+++ urfu_water_segmentation/landcover/dist_train.sh
@@ -3,13 +3,13 @@
 source ../.venv/bin/activate
 
 GPUS=2
-NODE_PARAMS="-p hiperf --gres=gpu:a100:$GPUS --nodelist=tesla-v100 -t 00:15:00"
+NODE_PARAMS="-p hiperf --gres=gpu:a100:$GPUS --nodelist=tesla-a100 -t 00:15:00"
 
 sbatch -n1 \
     --cpus-per-task=8 \
     --mem=45000 \
     $NODE_PARAMS \
-    --job-name=mmsegm-water \
+    --job-name=mmsegm-tree \
     --ntasks=${GPUS} \
     --ntasks-per-node=${GPUS} \
     --wrap="python ./train.py ./configs_2025_05/config_trees_mask2former.py --launcher slurm"
