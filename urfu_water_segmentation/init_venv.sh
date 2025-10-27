9eec91d981a03760e66690101753cbb913e3ba76
Владислав Савенков <vladislavsavenkov@Vladislavs-MacBook-Pro.local>
2025-10-27 13:42:50 +0500

    attempt3

--- urfu_water_segmentation/init_venv.sh
+++ urfu_water_segmentation/init_venv.sh
@@ -7,7 +7,7 @@ pip install poetry==1.8.3
 
 poetry install
 
-pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116
+pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116 --default-timeout=1000
 
 pip install mmcv==2.0.0 -f https://download.openmmlab.com/mmcv/dist/cu116/torch1.13/index.html
 
