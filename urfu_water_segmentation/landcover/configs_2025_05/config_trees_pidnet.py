b97bf06d3b17c86dd8d5de8fa1afcc8a768051e7
Владислав Савенков <vladislavsavenkov@Vladislavs-MacBook-Pro.local>
2025-11-16 00:04:24 +0500

    feature

--- urfu_water_segmentation/landcover/configs_2025_05/config_trees_pidnet.py
+++ urfu_water_segmentation/landcover/configs_2025_05/config_trees_pidnet.py
@@ -154,7 +154,7 @@ param_scheduler = [
 ]
 
 # training schedule for 120k
-train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=max_epochs, val_interval=1, max_iters=None)
+train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=max_epochs, val_interval=1)
 val_cfg = dict(type='ValLoop')
 test_cfg = dict(type='TestLoop')
 
