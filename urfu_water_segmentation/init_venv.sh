5b986e54aa941054be4f5d1c842cb745b66cfc49
студент0039 <s0039@umtnew.imm.uran.ru>
2025-11-15 23:54:43 +0500

    feature

--- urfu_water_segmentation/init_venv.sh
+++ urfu_water_segmentation/init_venv.sh
@@ -5,9 +5,9 @@ source .venv/bin/activate
 pip install -U pip
 pip install poetry==1.8.3
 
-poetry install
+poetry install -vv
 
-pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116 --default-timeout=1000
+pip install torch==1.13.0 torchvision==0.14.0 torchaudio==0.13.0 --extra-index-url https://download.pytorch.org/whl/cu116
 
 pip install mmcv==2.0.0 -f https://download.openmmlab.com/mmcv/dist/cu116/torch1.13/index.html
 
