aefda6c32f9b65a5772a3753de2006e916b62428
Владислав Савенков <vladislavsavenkov@Vladislavs-MacBook-Pro.local>
2025-11-16 00:01:10 +0500

    feature

--- configs/pidnet/pidnet-s_2xb6-120k_1024x1024-cityscapes.py
+++ configs/pidnet/pidnet-s_2xb6-120k_1024x1024-cityscapes.py
@@ -82,32 +82,32 @@ train_pipeline = [
 ]
 train_dataloader = dict(batch_size=6, dataset=dict(pipeline=train_pipeline))
 
-iters = 120000
-# optimizer
-optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
-optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer, clip_grad=None)
-# learning policy
-param_scheduler = [
-    dict(
-        type='PolyLR',
-        eta_min=0,
-        power=0.9,
-        begin=0,
-        end=iters,
-        by_epoch=False)
-]
-# training schedule for 120k
-train_cfg = dict(
-    type='IterBasedTrainLoop', max_iters=iters, val_interval=iters // 10)
-val_cfg = dict(type='ValLoop')
-test_cfg = dict(type='TestLoop')
-default_hooks = dict(
-    timer=dict(type='IterTimerHook'),
-    logger=dict(type='LoggerHook', interval=50, log_metric_by_epoch=False),
-    param_scheduler=dict(type='ParamSchedulerHook'),
-    checkpoint=dict(
-        type='CheckpointHook', by_epoch=False, interval=iters // 10),
-    sampler_seed=dict(type='DistSamplerSeedHook'),
-    visualization=dict(type='SegVisualizationHook'))
+# iters = 120000
+# # optimizer
+# optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
+# optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer, clip_grad=None)
+# # learning policy
+# param_scheduler = [
+#     dict(
+#         type='PolyLR',
+#         eta_min=0,
+#         power=0.9,
+#         begin=0,
+#         end=iters,
+#         by_epoch=False)
+# ]
+# # training schedule for 120k
+# train_cfg = dict(
+#     type='IterBasedTrainLoop', max_iters=iters, val_interval=iters // 10)
+# val_cfg = dict(type='ValLoop')
+# # test_cfg = dict(type='TestLoop')
+# default_hooks = dict(
+#     timer=dict(type='IterTimerHook'),
+#     logger=dict(type='LoggerHook', interval=50, log_metric_by_epoch=False),
+#     param_scheduler=dict(type='ParamSchedulerHook'),
+#     checkpoint=dict(
+#         type='CheckpointHook', by_epoch=False, interval=iters // 10),
+#     sampler_seed=dict(type='DistSamplerSeedHook'),
+#     visualization=dict(type='SegVisualizationHook'))
 
 randomness = dict(seed=304)
