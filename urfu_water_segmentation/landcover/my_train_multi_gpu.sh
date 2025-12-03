5824bd7f0a2648af2f4ebba8010df759c5704459
Владислав Савенков <vladislavsavenkov@Vladislavs-MacBook-Pro.local>
2025-12-04 01:52:30 +0500

    initial_branch

--- urfu_water_segmentation/landcover/my_train_multi_gpu.sh
+++ urfu_water_segmentation/landcover/my_train_multi_gpu.sh
@@ -6,3 +6,5 @@ sbatch \
   --cpus-per-task=8 --mem=45G -t 9:59:59 -J mmsegm-tree \
   --wrap="srun torchrun --nproc_per_node=8 --master_port=23456 \
           ./train.py ./configs_2025_05/config_trees_mask2former.py --launcher pytorch"
+
+
