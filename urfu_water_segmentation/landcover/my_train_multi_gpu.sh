f4099cfd3b89ce06947f6a3da1b4466b6ca3c060
Владислав Савенков <vladislavsavenkov@Vladislavs-MacBook-Pro.local>
2025-12-04 02:01:57 +0500

    config trees knet

--- urfu_water_segmentation/landcover/my_train_multi_gpu.sh
+++ urfu_water_segmentation/landcover/my_train_multi_gpu.sh
@@ -5,6 +5,6 @@ sbatch \
   --nodes=1 --gres=gpu:v100:8 \
   --cpus-per-task=8 --mem=45G -t 9:59:59 -J mmsegm-tree \
   --wrap="srun torchrun --nproc_per_node=8 --master_port=23456 \
-          ./train.py ./configs_2025_05/config_trees_mask2former.py --launcher pytorch"
+          ./train.py ./trees/config_trees_knet.py --launcher pytorch"
 
 
