ffd2375a5ff4f44938e498fbb57bf9a2ecf9defe
Владислав Савенков <vladislavsavenkov@Vladislavs-MacBook-Pro.local>
2025-12-04 23:02:34 +0500

    knet config

--- urfu_water_segmentation/landcover/trees/config_trees_knet.py
+++ urfu_water_segmentation/landcover/trees/config_trees_knet.py
@@ -1,7 +1,7 @@
 # для выбора модели, расписания необходимо наследовать один из файлов из репозитория mmsegmentation
 # базовые файлы для наследования можно посмотреть по пути mmsegmentation/configs/_base_/
 _base_ = [
-    '../../../configs/knet/knet-s3_swin-t_upernet_8xb2-adamw-80k_ade20k-512x512.py',
+    '../../../configs/knet/knet-s3_r50-d8_upernet_8xb2-adamw-80k_ade20k-512x512.py',
 ]
 
 default_scope = 'mmseg'
@@ -26,7 +26,7 @@ default_hooks = dict(
 # Изменение гиперпараметров
 
 # Если используем один графический процессор - BN, иначе - SyncBN
-norm_cfg = dict(type='BN', requires_grad=True)
+norm_cfg = dict(type='SyncBN', requires_grad=True)
 # Название датасета из файла urfu_project/dataset.py
 dataset_type = 'TreesDataset'
 # Путь к папке с преобразованным набором данных
@@ -43,7 +43,7 @@ loss = [
         dict(type='CrossEntropyLoss', loss_weight=1.0),
     ]
 # Размер батча
-batch_size = 4
+batch_size = 16
 gradient_accumulation_steps = 8
 actual_batch_size = batch_size * gradient_accumulation_steps
 # num_workers
@@ -62,26 +62,42 @@ log_interval = 10  # интервал в итерациях для печати 
 # Директория, где хрянятся файлы с списком изображений train и val
 splits = 'splits'
 
-
-pretrained = 'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/swin/swin_tiny_patch4_window7_224_20220308-f41b89d3.pth'  # noqa
-
 depths = [2, 2, 18, 2]
 num_stages = 3        # как в базовом конфиге
 conv_kernel_size = 1  # как в базовом конфиге
 num_classes = 2
 
-model = dict(
-    # оставляем Swin-бекбон, только глубины меняем
-    backbone=dict(
-        depths=depths,
-    ),
+data_preprocessor = dict(
+    type='SegDataPreProcessor',
+    mean=[123.675, 116.28, 103.53],
+    std=[58.395, 57.12, 57.375],
+    bgr_to_rgb=True,
+    pad_val=0,
+    size=crop_size,
+    seg_pad_val=255)
 
+model = dict(
+    type='EncoderDecoder',
+    data_preprocessor=data_preprocessor,
+    pretrained='open-mmlab://resnet50_v1c',
+    backbone=dict(
+        type='ResNetV1c',
+        depth=50,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        dilations=(1, 1, 1, 1),
+        strides=(1, 2, 2, 2),
+        norm_cfg=norm_cfg,
+        norm_eval=False,
+        style='pytorch',
+        contract_dilation=True),
     decode_head=dict(
-        # 1) Полностью переопределяем kernel_update_head
+        type='IterativeDecodeHead',
+        num_stages=num_stages,
         kernel_update_head=[
             dict(
                 type='KernelUpdateHead',
-                num_classes=num_classes,       
+                num_classes=num_classes,
                 num_ffn_fcs=2,
                 num_heads=8,
                 num_mask_fcs=1,
@@ -93,71 +109,51 @@ model = dict(
                 ffn_act_cfg=dict(type='ReLU', inplace=True),
                 with_ffn=True,
                 feat_transform_cfg=dict(
-                    conv_cfg=dict(type='Conv2d'),
-                    act_cfg=None,
-                ),
+                    conv_cfg=dict(type='Conv2d'), act_cfg=None),
                 kernel_updator_cfg=dict(
                     type='KernelUpdator',
                     in_channels=256,
                     feat_channels=256,
                     out_channels=256,
                     act_cfg=dict(type='ReLU', inplace=True),
-                    norm_cfg=dict(type='LN'),
-                ),
-            )
-            for _ in range(num_stages)
+                    norm_cfg=dict(type='LN'))) for _ in range(num_stages)
         ],
-
-        # 2) А здесь **дописываем** нужные поля, остальное тянем из базы
         kernel_generate_head=dict(
+            type='UPerHead',
+            in_channels=[256, 512, 1024, 2048],
+            in_index=[0, 1, 2, 3],
+            pool_scales=(1, 2, 3, 6),
+            channels=512,
+            dropout_ratio=0.1,
             num_classes=num_classes,
-            # можешь оставить один CrossEntropyLoss, если не зайдёт MSE
-            loss_decode=[
-                dict(type='CrossEntropyLoss', loss_weight=1.0)
-            ],
-        ),
-    ),
-
-    # 3) Auxiliary head тоже переводим на 2 класса
+            norm_cfg=norm_cfg,
+            align_corners=False,
+            loss_decode=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0))),
     auxiliary_head=dict(
+        type='FCNHead',
+        in_channels=1024,
+        in_index=2,
+        channels=256,
+        num_convs=1,
+        concat_input=False,
+        dropout_ratio=0.1,
         num_classes=num_classes,
+        norm_cfg=norm_cfg,
+        align_corners=False,
         loss_decode=dict(
-            type='CrossEntropyLoss',
-            loss_weight=0.4,
-        ),
-    ),
-)
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4)),
+    # model training and testing settings
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole'))
 
-# set all layers in backbone to lr_mult=0.1
-# set all norm layers, position_embeding,
-# query_embeding, level_embeding to decay_multi=0.0
-backbone_norm_multi = dict(lr_mult=0.1, decay_mult=0.0)
-backbone_embed_multi = dict(lr_mult=0.1, decay_mult=0.0)
-embed_multi = dict(lr_mult=1.0, decay_mult=0.0)
-custom_keys = {
-    'backbone': dict(lr_mult=0.1, decay_mult=1.0),
-    'backbone.patch_embed.norm': backbone_norm_multi,
-    'backbone.norm': backbone_norm_multi,
-    'absolute_pos_embed': backbone_embed_multi,
-    'relative_position_bias_table': backbone_embed_multi,
-    'query_embed': embed_multi,
-    'query_feat': embed_multi,
-    'level_embed': embed_multi
-}
-custom_keys.update({
-    f'backbone.stages.{stage_id}.blocks.{block_id}.norm': backbone_norm_multi
-    for stage_id, num_blocks in enumerate(depths)
-    for block_id in range(num_blocks)
-})
-custom_keys.update({
-    f'backbone.stages.{stage_id}.downsample.norm': backbone_norm_multi
-    for stage_id in range(len(depths) - 1)
-})
+
 # optimizer
 optim_wrapper = dict(
-    accumulative_counts=gradient_accumulation_steps,
-    paramwise_cfg=dict(custom_keys=custom_keys, norm_decay_mult=0.0),
-)
+    _delete_=True,
+    type='OptimWrapper',
+    optimizer=dict(type='AdamW', lr=0.0001, weight_decay=0.0005),
+    clip_grad=dict(max_norm=1, norm_type=2))
 
 train_pipeline = [
     dict(type='LoadImageFromFile'),
