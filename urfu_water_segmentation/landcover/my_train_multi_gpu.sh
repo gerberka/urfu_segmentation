162d2e478532c4716ffd761285b6c1593418de4a
Владислав Савенков <vladislavsavenkov@Vladislavs-MacBook-Pro.local>
2026-02-03 16:15:11 +0500

    run script

--- urfu_water_segmentation/landcover/my_train_multi_gpu.sh
+++ urfu_water_segmentation/landcover/my_train_multi_gpu.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 sbatch \
-  -p hiperf --nodelist=tesla-v100 \
-  --nodes=1 --gres=gpu:v100:8 \
+  -p hiperf --nodelist=tesla-a100 \
+  --nodes=1 --gres=gpu:a100:8 \
   --cpus-per-task=8 --mem=0 -t 9:59:59 -J mmsegm-tree \
   --wrap="srun torchrun --nproc_per_node=8 --master_port=23456 \
-          ./train.py ./trees/config.py --launcher pytorch"
+          ./train.py ./trees/config_trees_beit.py --launcher pytorch"
